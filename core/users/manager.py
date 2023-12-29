11847c60f0ee770cec0b9e03472e2ef64430cd67
Marcelo Livramento <molivramento@gmail.com>
2023-12-29 14:23:06 -0300

    fix users

---
+++ core/users/manager.py
@@ -0,0 +1,10 @@
+from cacau.database_manager import DatabaseManager
+from core.users.models import User
+
+
+class UserManager(DatabaseManager):
+    def __init__(self):
+        super().__init__(User)
+
+
+users = UserManager()
