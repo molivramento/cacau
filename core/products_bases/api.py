22fe0af0014d612b6f42995928d80eb9f5286cd7
Marcelo Livramento <molivramento@gmail.com>
2024-01-11 19:02:45 -0300

    changed from sync to async

--- core/products_bases/api.py
+++ core/products_bases/api.py
@@ -8,25 +8,25 @@ router = Router()
 
 
 @router.get("", response=list[ProductBaseOut])
-def get_all(request, filters: Query[ProductBaseFilters]):
-    return product_base_manager.get_all(filters)
+async def get_all(request, filters: Query[ProductBaseFilters]):
+    return await product_base_manager.get_all(filters)
 
 
 @router.get("/{uuid}", response=ProductBaseOut)
-def get_by_uuid(request, uuid: UUID):
-    return product_base_manager.get_by_uuid(uuid)
+async def get_by_uuid(request, uuid: UUID):
+    return await product_base_manager.get_by_uuid(uuid)
 
 
 @router.post("", response=ProductBaseOut)
-def create(request, product_base: ProductBaseCreate):
-    return product_base_manager.create(product_base)
+async def create(request, product_base: ProductBaseCreate):
+    return await product_base_manager.create(product_base)
 
 
 @router.put("/{uuid}", response=ProductBaseOut)
-def update(request, uuid: UUID, product_base: ProductBaseUpdate):
-    return product_base_manager.update(uuid, product_base)
+async def update(request, uuid: UUID, product_base: ProductBaseUpdate):
+    return await product_base_manager.update(uuid, product_base)
 
 
 @router.delete("/{uuid}")
-def delete(request, uuid: UUID):
-    return product_base_manager.delete(uuid)
+async def delete(request, uuid: UUID):
+    return await product_base_manager.delete(uuid)
